9db8bc24ad16f8749dc15f3eb1923bb60eed8eb4
minseo2222 <hiyoukou@naver.com>
2024-05-28 18:42:10 +0900

    5

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,5 +1,25 @@
 const Resume = () => {
-  return <div>나는 프로젝트 내역</div>;
+  return (
+    <div className="resume-container">
+      <h1>소개</h1>
+      <section className="section">
+        <h2>2024 실전코딩 수강</h2>
+        <p>2024년도 실전코딩을 수강하면서 실무에서 직접 사용하는 스킬들을 배웠습니다.</p>
+      </section>
+
+      <section className="section">
+        <h2>k-11, k-12 해커톤 등 여러 대회 참여</h2>
+        <p>대회를 통해 팀프로젝트와 여러 스킬들을 배웠습니다.</p>
+      </section>
+
+      <section className="section">
+        <h2>교육</h2>
+        <ul>
+          <li>2024년: 충남대학교 컴퓨터공학과 재학중</li>
+        </ul>
+      </section>
+    </div>
+  );
 };
 
 export default Resume;
